64c5f5c644f05f90e7150a5eeeca0c926f7a33ce
Emil Kampp <emil@kampp.me>
2018-04-06 10:37:56 +0200

    Extract rendering to seperate method for calling directly

--- lib/http_error/response.rb
+++ lib/http_error/response.rb
@@ -5,16 +5,21 @@ module HttpError
     included do
       rescue_from HttpError::Error do |error|
         Rails.logger.debug "Rescuing from #{error.class}. Status: #{error.status}. Message: #{error.message}"
-
-        hash = {
-          error: error.message,
-        }
-
-        render \
-          text: error.message,
-          json: hash,
-          status: error.status
+        render_error error
       end
+
+      private
+
+        def render_error(error)
+          hash = {
+            error: error.message,
+          }
+
+          render \
+            text: error.message,
+            json: hash,
+            status: error.status
+        end
     end
   end
 end
